b2375223f1d4b59fda595014fd85f04d82ff13af
shogoisaji <simacsimac9@gmail.com>
2023-11-05 17:38:14 +0900

    first

---
+++ app/models/users.go
@@ -0,0 +1,142 @@
+package models
+
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	ID        int
+	UUID      string
+	Name      string
+	Email     string
+	PassWord  string
+	CreatedAt time.Time
+	Todos     []Todo
+}
+
+type Session struct {
+	ID        int
+	UUID      string
+	Email     string
+	UserID    int
+	CreatedAt time.Time
+}
+
+func (u *User) CreateUser() (err error) {
+	cmd := `INSERT INTO users (
+		uuid,
+		name,
+		email,
+		password,
+		created_at) VALUES (?, ?, ?, ?, ?)`
+	_, err = Db.Exec(cmd,
+		createUUID(), u.Name, u.Email, Encrypt(u.PassWord), time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetUser(id int) (user User, err error) {
+	user = User{}
+	cmd := `SELECT * FROM users WHERE id = ?`
+	err = Db.QueryRow(cmd, id).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt)
+	return user, err
+}
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `UPDATE users SET name = ?, email = ? WHERE id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `DELETE FROM users WHERE id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func GetUserByEmail(email string) (user User, err error) {
+	user = User{}
+	cmd := `SELECT id, uuid, name, email, password, created_at FROM users WHERE email = ?`
+	err = Db.QueryRow(cmd, email).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt)
+	return user, err
+}
+
+func (u *User) CreateSession() (session Session, err error) {
+	session = Session{}
+	cmd1 := `INSERT INTO sessions (uuid, email, user_id, created_at) VALUES (?, ?, ?, ?)`
+	_, err = Db.Exec(cmd1, createUUID(), u.Email, u.ID, time.Now())
+	if err != nil {
+		log.Println(err)
+	}
+
+	cmd2 := `SELECT id, uuid, email, user_id, created_at FROM sessions WHERE user_id = ? AND email = ?`
+	err = Db.QueryRow(cmd2, u.ID, u.Email).Scan(
+		&session.ID,
+		&session.UUID,
+		&session.Email,
+		&session.UserID,
+		&session.CreatedAt)
+	return session, err
+}
+
+func (session *Session) CheckSession() (valid bool, err error) {
+	cmd := `SELECT id, uuid, email, user_id, created_at FROM sessions WHERE uuid = ?`
+	err = Db.QueryRow(cmd, session.UUID).Scan(
+		&session.ID,
+		&session.UUID,
+		&session.Email,
+		&session.UserID,
+		&session.CreatedAt)
+	if err != nil {
+		valid = false
+		return
+	}
+	if session.ID != 0 {
+		valid = true
+	}
+	return valid, err
+
+}
+
+func (session *Session) DeleteSessionByUUID() (err error) {
+	cmd := `DELETE FROM sessions WHERE uuid = ?`
+	_, err = Db.Exec(cmd, session.UUID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (sess *Session) GetUserBySession() (user User, err error) {
+	user = User{}
+	cmd := `SELECT id, uuid, name, email, password, created_at FROM users WHERE id = ?`
+	err = Db.QueryRow(cmd, sess.UserID).Scan(
+		&user.ID,
+		&user.UUID,
+		&user.Name,
+		&user.Email,
+		&user.PassWord,
+		&user.CreatedAt)
+	return user, err
+}
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"todo_app_go/app/models"
+	"todo_app_go/app/models/controllers"
+)
+
+func main() {
+	fmt.Println(models.Db)
+
+	controllers.StartMainServer()
+
+	// user, _ := models.GetUserByEmail("test@example.com")
+	// fmt.Println(user)
+
+	// session, err := user.CreateSession()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// fmt.Println(session)
+
+	// valid, _ := session.CheckSession()
+	// fmt.Println(valid)
+
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("test")
+
+	// u := &models.User{}
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.Password = "testtest"
+	// fmt.Println(u)
+
+	// u.CreateUser()
+
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
+	// u.Name = "Test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// user, _ := models.GetUser(3)
+	// user.CreateTodo("third todo")
+
+	// t, _ := models.GetTodo(1)
+	// fmt.Println(t)
+
+	// todos, _ := models.GetTodos()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// user2, _ := models.GetUser(3)
+	// todos, _ := user2.GetTodosByUser()
+	// for _, v := range todos {
+	// 	fmt.Println(v)
+	// }
+
+	// t, _ := models.GetTodo(1)
+	// t.Content = "Update Todo"
+	// t.UpdateTodo()
+
+	// t, _ := models.GetTodo(3)
+	// t.DeleteTodo()
+
+}
